9ba2aeac202eac4a0c5a0ee6591e9d9b7be313fb
hoangnv170752 <hoang.nv.ral@gmail.com>
2024-09-22 18:28:51 +0700

    init: create project

---
+++ frontend/src/pages/MyProfile.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const MyProfile = () => {
+  return (
+    <div>
+        My Profile
+    </div>
+  )
+}
+
+export default MyProfile
